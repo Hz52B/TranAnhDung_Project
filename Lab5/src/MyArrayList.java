951218ad74dbd9070d3e8ef6850171a13d86a519
Hz52B <tanre43@gmail.com>
2023-05-24 22:26:17 +0700

    Lab5

---
+++ Lab5/src/MyArrayList.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class MyArrayList {
+	public static void main(String[] args) {
+		ArrayList<Object> ArrL = new ArrayList<>();
+		
+		ArrL.add(100);
+		ArrL.add(3.14);
+		ArrL.add(true);
+		ArrL.add("Hello Kitty");
+		
+		for(Object value : ArrL) {
+			System.out.println(value);
+		}
+	}
+}
